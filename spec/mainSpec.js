70e72919b73a98d1423b90453182e9862838b145
alex <al3xback@gmail.com>
2024-01-18 09:05:24 +0700

    added first commit

---
+++ spec/mainSpec.js
@@ -0,0 +1,47 @@
+describe('DOM', () => {
+	beforeEach(() => {
+		document.body.insertAdjacentHTML('afterbegin', getBodyContent());
+	});
+
+	afterEach(function () {
+		document.body.removeChild(document.getElementById('wrapper'));
+	});
+
+	it('should have a number type of each card list image width and height attribute values', () => {
+		const cardImgEls = document.querySelectorAll('.card__image img');
+
+		for (let i = 0; i < cardImgEls.length; i++) {
+			const cardImgEl = cardImgEls[i];
+			const cardImgWidth = cardImgEl.width;
+			const cardImgHeight = cardImgEl.height;
+
+			expect(cardImgWidth).toBeInstanceOf(Number);
+			expect(cardImgHeight).toBeInstanceOf(Number);
+		}
+	});
+
+	it("should have a title element that contains 'Reliable, efficient delivery' word", () => {
+		const sectionTitleEl = document.querySelector('.section__title');
+		const sectionTitle = sectionTitleEl.textContent.trim();
+
+		expect(sectionTitle).toContain('Reliable, efficient delivery');
+	});
+
+	it('should have two children inside of the section element', () => {
+		const sectionEl = document.querySelector('section');
+		const sectionChildrenEls = sectionEl.children;
+
+		expect(sectionChildrenEls).toHaveSize(2);
+	});
+
+	it('should have an empty alt attribute value of each card list item image element', () => {
+		const cardListItemImgEls =
+			document.querySelectorAll('.card__image img');
+
+		for (let i = 0; i < cardListItemImgEls.length; i++) {
+			const cardListItemImgAlt = cardListItemImgEls[i].alt;
+
+			expect(cardListItemImgAlt).toBe('');
+		}
+	});
+});
